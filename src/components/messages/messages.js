68f9afaa00f6c8b18ae7f7ccc56ca73cb0103cca
MKrumscheid <136487365+MKrumscheid@users.noreply.github.com>
2024-07-02 19:45:35 +0200

    Revert "Update messages.js"
    This reverts commit e829347402f0a038c2c401ded99e4764eb523631.

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -101,9 +101,6 @@ const Messages = () => {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Cache-Control": "no-cache",
-            Pragma: "no-cache",
-            Expires: "0",
           },
         }
       );
@@ -133,6 +130,7 @@ const Messages = () => {
         (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
       );
       // Update the local storage and state with the unique messages
+
       const updatedMessages = removeOldMessages(uniqueMessages);
       setMessages(updatedMessages);
       localStorage.setItem("myMessages", JSON.stringify(updatedMessages));
