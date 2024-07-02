c350b76d5f6962911a3660f0160b17fe3c1c4dc1
MKrumscheid <136487365+MKrumscheid@users.noreply.github.com>
2024-07-02 22:57:53 +0200

    Update messages.js

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -16,7 +16,7 @@ const Messages = () => {
     const messages = JSON.parse(localStorage.getItem("myMessages")) || [];
     const currentTime = new Date().getTime();
     const filteredMessages = messages.filter(
-      (message) => currentTime - message.updatedAt < 12 * 60 * 60 * 1000
+      (message) => currentTime - message.createdAt < 12 * 60 * 60 * 1000
     );
     localStorage.setItem("myMessages", JSON.stringify(filteredMessages));
   };
@@ -42,7 +42,7 @@ const Messages = () => {
     const messages = JSON.parse(localStorage.getItem("myMessages")) || [];
     const updatedMessages = [
       ...messages,
-      { ...message, createdAt: new Date().getTime(), isMyMessage: true },
+      { ...message, createdAt: new Date().getTime() },
     ];
     localStorage.setItem("myMessages", JSON.stringify(updatedMessages));
   };
@@ -96,16 +96,9 @@ const Messages = () => {
   const fetchMessages = useCallback(async () => {
     removeOldMessages(); // Remove old messages before fetching new ones
     const localMessages = JSON.parse(localStorage.getItem("myMessages")) || [];
-    const currentTime = Date.now();
-    const twelveHoursInMillis = 12 * 60 * 60 * 1000;
-
-    // Filter out local messages older than 12 hours
-    const filteredMessages = localMessages.filter(
-      (message) => currentTime - message.createdAt < twelveHoursInMillis
-    );
-
     try {
       const response = await fetch(
+        // `http://localhost:3000/api/messages/nearby?latitude=${location.latitude}&longitude=${location.longitude}`,
         `https://nearyou-server-28246f0c9e39.herokuapp.com/api/messages/nearby?latitude=${location.latitude}&longitude=${location.longitude}`,
         {
           method: "GET",
@@ -116,32 +109,16 @@ const Messages = () => {
       );
       const data = await response.json();
 
-      // Add isMyMessage attribute to server messages based on localMessages
-      const allMessages = data.map((message) => {
-        const localMessage = localMessages.find((m) => m.id === message.id);
-        return localMessage
-          ? { ...message, isMyMessage: localMessage.isMyMessage }
-          : message;
-      });
-
-      // Combine filtered local messages and server messages
-      const combinedMessages = [...filteredMessages, ...allMessages];
-
-      // Remove duplicate messages and keep isMyMessage attribute
-      const uniqueMessages = combinedMessages.reduce((acc, current) => {
-        const existingMessage = acc.find((item) => item.id === current.id);
-        if (!existingMessage) {
+      const allMessages = [...localMessages, ...data];
+      // Remove duplicate messages, since the server might return messages that are already in the local storage
+      const uniqueMessages = allMessages.reduce((acc, current) => {
+        const x = acc.find((item) => item.id === current.id);
+        if (!x) {
           return acc.concat([current]);
         } else {
-          // Keep isMyMessage attribute from local message
-          return acc.map((item) =>
-            item.id === current.id
-              ? { ...current, isMyMessage: existingMessage.isMyMessage }
-              : item
-          );
+          return acc;
         }
       }, []);
-
       // Calculate the distance of each message from the user's location
       uniqueMessages.forEach((msg) => {
         msg.distance = calculateDistance(
@@ -151,12 +128,10 @@ const Messages = () => {
           msg.location.coordinates[0] // Longitude
         );
       });
-
       // Sort the messages by creation date to show them in the correct order
       uniqueMessages.sort(
         (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
       );
-
       // Update the local storage and state with the unique messages
       localStorage.setItem("myMessages", JSON.stringify(uniqueMessages));
       setMessages(uniqueMessages);
