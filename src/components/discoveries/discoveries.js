9300f7b56c12ba5eaed06b78623b42f251adf0e9
MKrumscheid <136487365+MKrumscheid@users.noreply.github.com>
2024-07-02 12:29:24 +0200

    0.452

--- src/components/discoveries/discoveries.js
+++ src/components/discoveries/discoveries.js
@@ -7,14 +7,13 @@ import DistanceDropdown from "../drop_downs/DistanceDropdown";
 import CategoryDropdown from "../drop_downs/CategoryDropdown";
 
 const Discoveries = () => {
-  //setting the states for the coupons, distance, selected categories, and the dropdowns. We use these for interaction with the user
-  const [coupons, setCoupons] = useState([]); //keeping track of the coupons
-  const [distance, setDistance] = useState(3000); //setting the distance to 3000 meters as default, can be changed by the user via the dropdown
-  const [selectedCategories, setSelectedCategories] = useState([]); //setting the selected categories to an empty array, can be changed by the user via the dropdown
-  const [showDistanceDropdown, setShowDistanceDropdown] = useState(false); //dont show the distance dropdown by default
-  const [showCategoryDropdown, setShowCategoryDropdown] = useState(false); //dont show the category dropdown by default
-  const location = useGeolocation(); //using the geolocation hook to get the location of the user
-  const navigate = useNavigate(); //using the navigate hook to navigate between the different pages
+  const [coupons, setCoupons] = useState([]);
+  const [distance, setDistance] = useState(3000);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [showDistanceDropdown, setShowDistanceDropdown] = useState(false);
+  const [showCategoryDropdown, setShowCategoryDropdown] = useState(false);
+  const location = useGeolocation();
+  const navigate = useNavigate();
 
   // Function to get the presigned URL for a file from S3
   const getPresignedUrl = async (key) => {
@@ -30,27 +29,30 @@ const Discoveries = () => {
     }
   };
 
-  //fetching the coupons from the backend via the API
+  // Fetching the coupons from the backend via the API
   const fetchCoupons = useCallback(async () => {
-    //using the useCallback hook to memoize the function and only call it when the dependencies change (dependencies are distance, location.latitude and location.longitude)
     try {
       const response = await fetch(
         `https://nearyou-server-28246f0c9e39.herokuapp.com/api/coupons/nearby/?distance=${distance}&latitude=${location.latitude}&longitude=${location.longitude}`
       );
-      const data = await response.json(); //data now contains all nearby coupons send from the server
+      const data = await response.json();
 
-      //getting the stored coupons from the local storage or an empty array if there are none
+      // Fetch presigned URLs for company logos and product photos
+      const updatedCoupons = await Promise.all(
+        data.map(async (coupon) => {
+          const companyLogoUrl = await getPresignedUrl(coupon.companyLogo);
+          const productPhotoUrl = await getPresignedUrl(coupon.productPhoto);
+          return {
+            ...coupon,
+            companyLogo: companyLogoUrl,
+            productPhoto: productPhotoUrl,
+          };
+        })
+      );
+
       const storedCoupons = JSON.parse(localStorage.getItem("coupons")) || [];
       const currentTime = Date.now();
 
-      // Filter presigned URLs for company logos and product photos
-      const updatedCoupons = data.map((coupon) => ({
-        ...coupon,
-        companyLogo: coupon.companyLogoUrl,
-        productPhoto: coupon.productPhotoUrl,
-      }));
-
-      //filtering out the valid claimed coupons and adding the remaining validity to them
       const validClaimedCoupons = storedCoupons
         .filter((coupon) => coupon.claimed)
         .map((coupon) => {
@@ -63,12 +65,9 @@ const Discoveries = () => {
         })
         .filter((coupon) => coupon.remainingValidity > 0);
 
-      //merging the updated coupons with the stored coupons and the valid claimed coupons
       const mergedCoupons = [
         ...updatedCoupons.map((coupon) => {
-          //mapping over the updated coupons and adding the quantity, claimed, validity, remaining validity and claimedAt to them
           const storedCoupon = storedCoupons.find((c) => c.id === coupon.id);
-          //if the stored coupon exists, return the updated coupon with the quantity, claimed, validity, remaining validity and claimedAt from the stored coupon
           if (storedCoupon) {
             return {
               ...coupon,
@@ -82,29 +81,26 @@ const Discoveries = () => {
               claimedAt: storedCoupon.claimedAt,
             };
           }
-          //if the stored coupon does not exist, return the coupon with the remaining validity added
           return {
             ...coupon,
             remainingValidity: coupon.validity * 60000,
           };
         }),
-        //filtering out the valid claimed coupons that are not in the updated coupons to then also merge them, since claimed coupons should always be shown, even if they are out of the specified distance
         ...validClaimedCoupons.filter(
           (claimedCoupon) =>
-            !updatedCoupons.some((coupon) => coupon.id === claimedCoupon.id) //if the updated coupons do not contain the claimed coupon, return the claimed coupon
+            !updatedCoupons.some((coupon) => coupon.id === claimedCoupon.id)
         ),
       ];
 
-      setCoupons(mergedCoupons); //setting a new state for the coupons with the merged coupons
-      localStorage.setItem("coupons", JSON.stringify(mergedCoupons)); //storing the merged coupons in the local storage
+      setCoupons(mergedCoupons);
+      localStorage.setItem("coupons", JSON.stringify(mergedCoupons));
     } catch (error) {
       console.error("Error fetching coupons:", error);
     }
-  }, [distance, location.latitude, location.longitude]); //dependencies are distance, location.latitude and location.longitude, this function will only be called when these dependencies change
+  }, [distance, location.latitude, location.longitude]);
 
   const claimCoupon = async (id) => {
     try {
-      //using a PATCH request to claim the coupon with the specified id
       const response = await fetch(
         `https://nearyou-server-28246f0c9e39.herokuapp.com/api/coupons/${id}`,
         {
@@ -117,12 +113,11 @@ const Discoveries = () => {
       );
       const data = await response.json();
       const currentTime = Date.now();
-      //mapping over the coupons (in the state) and updating the patched coupon with the specified id (the patched coupon is the one being claimed)
       const updatedCoupons = coupons.map((coupon) =>
         coupon.id === id
           ? {
               ...coupon,
-              quantity: data.quantity, //updating the quantity of the coupon, the Server decreases the quantity by 1 when the coupon is PATCHED
+              quantity: data.quantity,
               claimed: true,
               remainingValidity: coupon.validity * 60000,
               claimedAt: currentTime,
@@ -133,66 +128,55 @@ const Discoveries = () => {
       setCoupons(updatedCoupons);
       localStorage.setItem("coupons", JSON.stringify(updatedCoupons));
 
-      //getting the claimed coupon from the updated coupons
       const claimedCoupon = updatedCoupons.find((coupon) => coupon.id === id);
 
-      //if the claimed coupon exists and the location of the user is known, open the route service with the location of the user and the location of the claimed coupon
       if (claimedCoupon && location.latitude && location.longitude) {
         const { latitude: userLat, longitude: userLng } = location;
         const [couponLng, couponLat] = claimedCoupon.location.coordinates;
         const openRouteServiceUrl = `https://maps.openrouteservice.org/directions?n1=${userLat}&n2=${userLng}&n3=14&a=${userLat},${userLng},${couponLat},${couponLng}&b=0&c=0&k1=en-US&k2=km`;
-        window.open(openRouteServiceUrl, "_blank"); //open the route service in a new tab
+        window.open(openRouteServiceUrl, "_blank");
       }
     } catch (error) {
       console.error("Error claiming coupon:", error);
     }
   };
 
-  //using the useEffect hook to fetch the coupons when the location of the user is known
   useEffect(() => {
     if (location.latitude && location.longitude) {
       fetchCoupons();
     }
-  }, [location, distance, selectedCategories, fetchCoupons]); //dependencies are location, distance, selected categories and fetch coupons
+  }, [location, distance, selectedCategories, fetchCoupons]);
 
-  //using the useEffect hook to fetch the coupons when the component is mounted (on page load)
   useEffect(() => {
     fetchCoupons();
   }, [fetchCoupons]);
 
-  //functions to toggle the dropdowns and apply the filters
   const toggleDistanceDropdown = () => {
     setShowDistanceDropdown(!showDistanceDropdown);
     setShowCategoryDropdown(false);
   };
 
-  //function to toggle the category dropdown
   const toggleCategoryDropdown = () => {
     setShowCategoryDropdown(!showCategoryDropdown);
     setShowDistanceDropdown(false);
   };
 
-  //function to apply the distance filter
   const applyDistanceFilter = (newDistance) => {
     setDistance(newDistance);
     setShowDistanceDropdown(false);
   };
 
-  //function to apply the category filter
   const applyCategoryFilter = (categories) => {
     setSelectedCategories(categories);
     setShowCategoryDropdown(false);
   };
 
-  //filtering the coupons based on the selected categories
-  //if the selected categories are empty, return all coupons, else return the coupons that have the product category included in the selected categories
   const filteredCoupons = selectedCategories.length
-    ? coupons.filter(
-        (coupon) => selectedCategories.includes(coupon.productCategory) //if the selected categories include the product category of the coupon, return the coupon
+    ? coupons.filter((coupon) =>
+        selectedCategories.includes(coupon.productCategory)
       )
     : coupons;
 
-  //returning the JSX for the discoveries component
   return (
     <div className="discover-container">
       <div className="left-column">
